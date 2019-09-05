e6b685a06b74eb61c72b0b53edb02c660160cf0d
munyolec <33727407+munyolec@users.noreply.github.com>
2019-09-05 12:04:56 +0300

    more

---
+++ listings/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Listing
+
+# Create your views here.
+def index(request):
+   listings = Listing.objects.all()
+   print(listings)
+   context = {
+      'listings': listings
+   }
+   return render(request,'listings/listings.html') 
+
+def listing(request):
+   return render(request,'listings/listing.html')
+
+def search(request):
+  return  render(request,'listings/search.html')
